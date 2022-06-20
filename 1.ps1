5653535ebe11a53cda47a4ca25585aa4027971a1
Mahesh <chukkasaimahesh@gmail.com>
2022-06-20 15:00:55 +0530

    new updation

---
+++ 1.ps1
@@ -0,0 +1,17 @@
+﻿function A{
+    Param($functionToCall)
+    Write-Host "Calling function: $functionToCall"
+
+    # Obtain the scripts from the externally parsed function
+    $script=(Get-Item "function:$functionToCall").ScriptBlock
+
+    # invoking external function
+    [ScriptBlock]::Create($script).Invoke();
+
+}
+
+function B{
+    Write-Host "Function B has been invoked"
+}
+
+A -functionToCall B
